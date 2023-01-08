1f62e52fbecb8184ce3cb9eebeb76a02549d6288
Kureans <leonardtengys@gmail.com>
2023-01-08 19:24:42 +0800

    Add YS qn 125 & 226

---
+++ Week_1/125_Valid Palindrome_080123/YS_valid_palindrome.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.length()-1;
+        
+        while (left < right) {
+            while (left < right && !isalnum(s[left])) left++;
+            while (left < right && !isalnum(s[right])) right--;
+            if (tolower(s[left]) != tolower(s[right])) return false;
+            left++; right--;
+        }
+
+        return true;
+    }
+};
